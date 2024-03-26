c56f565864191870d58b2ec4c376a5957b7ac053
Phexyaa <32746604+Phexyaa@users.noreply.github.com>
2024-03-26 18:03:05 -0400

    Created employee factory

---
+++ EmpDemoApi/Data/EmployeeFactory.cs
@@ -0,0 +1,19 @@
+﻿using Shared.Models;
+
+namespace API.Data;
+
+public class EmployeeFactory : IEmployeeFactory
+{
+    public Person CreateEmployee(string firstName, string lastName, int salary, string title, DateTime hireDate, int age = 0)
+    {
+        return new Employee()
+        {
+            FirstName = firstName,
+            LastName = lastName,
+            Age = age,
+            Salary = salary,
+            Title = title,
+            HireDate = hireDate,
+        };
+    }
+}
---
+++ EmpDemoApi/Data/IEmployeeFactory.cs
@@ -0,0 +1,7 @@
+﻿using Shared.Models;
+
+namespace API.Data;
+public interface IEmployeeFactory
+{
+    Person CreateEmployee(string firstName, string lastName, int salary, string title, DateTime hireDate, int age = 0);
+}
