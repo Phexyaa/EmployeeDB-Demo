b757d7c707a83bf9d1b5c84be87cdd79993a862b
Phexyaa <32746604+Phexyaa@users.noreply.github.com>
2024-03-26 19:41:54 -0400

    Refactored data service so that methods return Employee objects instead of Person objects

---
+++ EmpDemoApi/Data/DataAccess.cs
@@ -0,0 +1,24 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Shared.Enums;
+using Shared.Models;
+using System.Data;
+
+namespace API.Data;
+
+public class DataAccess : IDataAccess
+{
+    private readonly IConfiguration _config;
+
+    public DataAccess(IConfiguration config)
+    {
+        _config = config;
+    }
+    internal IQueryable<Employee> GetAllEmployees()
+    {
+        using IDbConnection connection = new SqlConnection(_config.GetConnectionString("Default"));
+        var temp = connection.Query<Employee>("SpGetPeople", null, commandType: CommandType.StoredProcedure).AsQueryable();
+        return temp;
+    }
+
+}
---
+++ EmpDemoApi/Data/IDataAccess.cs
@@ -0,0 +1,8 @@
+﻿using Shared.Models;
+
+namespace API.Data;
+
+public interface IDataAccess
+{
+    IQueryable<Employee> GetPeople();
+}
