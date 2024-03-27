8aa97a9786e3a95561fe073811805aebdce80279
Phexyaa <32746604+Phexyaa@users.noreply.github.com>
2024-03-27 13:42:25 -0400

    Changed naming of files from People to Employee

--- Database/Employee.sql
+++ Database/Employee.sql
@@ -3,5 +3,8 @@
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [FirstName] NVARCHAR(50) NOT NULL, 
     [LastName] NVARCHAR(50) NOT NULL, 
-    [Age] INT NOT NULL
-)
+    [Age] INT NULL, 
+    [Salary] INT NOT NULL, 
+    [HireDate] DATETIME NOT NULL
+, 
+    [EmpoloyeeId] NVARCHAR(50) NULL)
