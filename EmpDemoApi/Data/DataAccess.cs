870f28ca0c1be936b748a14f612acc347ff01469
Phexyaa <32746604+Phexyaa@users.noreply.github.com>
2024-03-27 16:18:38 -0400

    fixed stored procedure naming issue in database project as well as implemented database calls in DataAccess.cs

--- EmpDemoApi/Data/DataAccess.cs
+++ EmpDemoApi/Data/DataAccess.cs
@@ -10,36 +10,84 @@ public class DataAccess : IDataAccess
 {
     private readonly IConfiguration _config;
 
-    public DataAccess(IConfiguration config)
+    public DataAccess(IConfiguration config) => _config = config;
+    internal IQueryable<Employee>? GetAllActiveEmployees()
     {
-        _config = config;
+        using IDbConnection connection = new SqlConnection(_config.GetConnectionString("Default"));
+        var result = connection.Query<Employee>("SpGetAllActiveEmployees", true, null, commandType: CommandType.StoredProcedure).AsQueryable();
+        return result;
     }
     public IQueryable<Employee>? GetAllEmployees()
     {
         using IDbConnection connection = new SqlConnection(_config.GetConnectionString("Default"));
-        var temp = connection.Query<Employee>("SpGetPeople", null, commandType: CommandType.StoredProcedure).AsQueryable();
-        return temp;
+        var result = connection.Query<Employee>("SpGetAllEmployees", null, commandType: CommandType.StoredProcedure).AsQueryable();
+        return result;
     }
-
-    internal IQueryable<Employee>? GetSpecificEmployees(SearchCriteria criteria, string keyword)
+    internal IQueryable<Employee>? GetAllInactiveEmployees()
     {
-        return null;
-        switch (criteria)
-        {
-            case SearchCriteria.FirstName:
-                break;
-            case SearchCriteria.LastName:
-                break;
-            case SearchCriteria.HireDate:
-                break;
-            case SearchCriteria.Age:
-                break;
-            case SearchCriteria.Title:
-                break;
-            case SearchCriteria.Salary:
-                break;
-            default:
-                throw new ArgumentOutOfRangeException(nameof(criteria));
-        }
+        using IDbConnection connection = new SqlConnection(_config.GetConnectionString("Default"));
+        var result = connection.Query<Employee>("SpGetAllInactiveEmployees", false, null, commandType: CommandType.StoredProcedure).AsQueryable();
+        return result;
+    }
+    internal IQueryable<Employee>? GetEmployeeByEmployeeId(Guid id)
+    {
+        using IDbConnection connection = new SqlConnection(_config.GetConnectionString("Default"));
+        var result = connection.Query<Employee>("SpGetEmployeesByEmployeeId", id, null, commandType: CommandType.StoredProcedure).AsQueryable();
+        return result;
+    }
+    internal IQueryable<Employee>? GetEmployeeByDatabaseId(int databaseId)
+    {
+        using IDbConnection connection = new SqlConnection(_config.GetConnectionString("Default"));
+        var result = connection.Query<Employee>("SpGetEmployeeById", databaseId, null, commandType: CommandType.StoredProcedure).AsQueryable();
+        return result;
+    }
+    internal IQueryable<Employee>? GetEmployeesByAge(int age)
+    {
+        using IDbConnection connection = new SqlConnection(_config.GetConnectionString("Default"));
+        var result = connection.Query<Employee>("SpGetEmployeesByAge", age, null, commandType: CommandType.StoredProcedure).AsQueryable();
+        return result;
+    }
+    internal IQueryable<Employee>? GetEmployeesByHireDate(DateTime hireDate)
+    {
+        using IDbConnection connection = new SqlConnection(_config.GetConnectionString("Default"));
+        var result = connection.Query<Employee>("SpGetEmployeesByHireDate", hireDate.ToShortDateString(), null, commandType: CommandType.StoredProcedure).AsQueryable();
+        return result;
+    }
+    internal IQueryable<Employee>? GetEmployeesByName(string? firstName = null, string? lastName = null)
+    {
+        var parameters = new string?[] { firstName, lastName };
+        using IDbConnection connection = new SqlConnection(_config.GetConnectionString("Default"));
+        var result = connection.Query<Employee>("SpGetEmployeesByName", parameters, null, commandType: CommandType.StoredProcedure).AsQueryable();
+        return result;
+    }
+    internal IQueryable<Employee>? GetEmployeesBySalary(decimal salary)
+    {
+        using IDbConnection connection = new SqlConnection(_config.GetConnectionString("Default"));
+        var result = connection.Query<Employee>("SpGetEmployeesBySalary", salary, null, commandType: CommandType.StoredProcedure).AsQueryable();
+        return result;
+    }
+    internal IQueryable<Employee>? GetEmployeesByTitle(string title)
+    {
+        using IDbConnection connection = new SqlConnection(_config.GetConnectionString("Default"));
+        var result = connection.Query<Employee>("SpGetEmployeesByTitle", title, null, commandType: CommandType.StoredProcedure).AsQueryable();
+        return result;
+    }
+    internal int InsertEmployee(Employee employee)
+    {
+        using IDbConnection connection = new SqlConnection(_config.GetConnectionString("Default"));
+        var result = connection.Execute("SpInsertEmployee", employee, null, commandType: CommandType.StoredProcedure);
+        return result;
+    }
+    internal int UpdateEmployee(Employee employee)
+    {
+        using IDbConnection connection = new SqlConnection(_config.GetConnectionString("Default"));
+        var result = connection.Execute("SpUpdateEmployee", employee, null, commandType: CommandType.StoredProcedure);
+        return result;
+    }
+    internal int DeleteEmployeeRecord(int databaseId)
+    {
+        using IDbConnection connection = new SqlConnection(_config.GetConnectionString("Default"));
+        var result = connection.Execute("SpDeleteEmployeeRecord", databaseId, null, commandType: CommandType.StoredProcedure);
+        return result;
     }
 }
