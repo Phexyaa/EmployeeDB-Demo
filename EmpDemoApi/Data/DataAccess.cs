e7544cb70937ab3438bfd38adcef6233c5e171bc
Phexyaa <32746604+Phexyaa@users.noreply.github.com>
2024-03-27 18:24:02 -0400

    made new methods public

--- EmpDemoApi/Data/DataAccess.cs
+++ EmpDemoApi/Data/DataAccess.cs
@@ -11,7 +11,7 @@ public class DataAccess : IDataAccess
     private readonly IConfiguration _config;
 
     public DataAccess(IConfiguration config) => _config = config;
-    internal IQueryable<Employee>? GetAllActiveEmployees()
+    public IQueryable<Employee>? GetAllActiveEmployees()
     {
         using IDbConnection connection = new SqlConnection(_config.GetConnectionString("Default"));
         var result = connection.Query<Employee>("SpGetAllActiveEmployees", true, null, commandType: CommandType.StoredProcedure).AsQueryable();
@@ -23,68 +23,68 @@ public class DataAccess : IDataAccess
         var result = connection.Query<Employee>("SpGetAllEmployees", null, commandType: CommandType.StoredProcedure).AsQueryable();
         return result;
     }
-    internal IQueryable<Employee>? GetAllInactiveEmployees()
+    public IQueryable<Employee>? GetAllInactiveEmployees()
     {
         using IDbConnection connection = new SqlConnection(_config.GetConnectionString("Default"));
         var result = connection.Query<Employee>("SpGetAllInactiveEmployees", false, null, commandType: CommandType.StoredProcedure).AsQueryable();
         return result;
     }
-    internal IQueryable<Employee>? GetEmployeeByEmployeeId(Guid id)
+    public IQueryable<Employee>? GetEmployeeByEmployeeId(Guid id)
     {
         using IDbConnection connection = new SqlConnection(_config.GetConnectionString("Default"));
         var result = connection.Query<Employee>("SpGetEmployeesByEmployeeId", id, null, commandType: CommandType.StoredProcedure).AsQueryable();
         return result;
     }
-    internal IQueryable<Employee>? GetEmployeeByDatabaseId(int databaseId)
+    public IQueryable<Employee>? GetEmployeeByDatabaseId(int databaseId)
     {
         using IDbConnection connection = new SqlConnection(_config.GetConnectionString("Default"));
         var result = connection.Query<Employee>("SpGetEmployeeById", databaseId, null, commandType: CommandType.StoredProcedure).AsQueryable();
         return result;
     }
-    internal IQueryable<Employee>? GetEmployeesByAge(int age)
+    public IQueryable<Employee>? GetEmployeesByAge(int age, bool greaterThan = false, bool lessThan = false, bool equalTo = true)
     {
         using IDbConnection connection = new SqlConnection(_config.GetConnectionString("Default"));
         var result = connection.Query<Employee>("SpGetEmployeesByAge", age, null, commandType: CommandType.StoredProcedure).AsQueryable();
         return result;
     }
-    internal IQueryable<Employee>? GetEmployeesByHireDate(DateTime hireDate)
+    public IQueryable<Employee>? GetEmployeesByHireDate(DateTime hireDate, bool greaterThan, bool lessThan, bool equalTo = true)
     {
         using IDbConnection connection = new SqlConnection(_config.GetConnectionString("Default"));
         var result = connection.Query<Employee>("SpGetEmployeesByHireDate", hireDate.ToShortDateString(), null, commandType: CommandType.StoredProcedure).AsQueryable();
         return result;
     }
-    internal IQueryable<Employee>? GetEmployeesByName(string? firstName = null, string? lastName = null)
+    public IQueryable<Employee>? GetEmployeesByName(string? firstName = null, string? lastName = null)
     {
         var parameters = new string?[] { firstName, lastName };
         using IDbConnection connection = new SqlConnection(_config.GetConnectionString("Default"));
         var result = connection.Query<Employee>("SpGetEmployeesByName", parameters, null, commandType: CommandType.StoredProcedure).AsQueryable();
         return result;
     }
-    internal IQueryable<Employee>? GetEmployeesBySalary(decimal salary)
+    public IQueryable<Employee>? GetEmployeesBySalary(decimal salary, bool greaterThan, bool lessThan, bool equalTo = true)
     {
         using IDbConnection connection = new SqlConnection(_config.GetConnectionString("Default"));
         var result = connection.Query<Employee>("SpGetEmployeesBySalary", salary, null, commandType: CommandType.StoredProcedure).AsQueryable();
         return result;
     }
-    internal IQueryable<Employee>? GetEmployeesByTitle(string title)
+    public IQueryable<Employee>? GetEmployeesByTitle(string title)
     {
         using IDbConnection connection = new SqlConnection(_config.GetConnectionString("Default"));
         var result = connection.Query<Employee>("SpGetEmployeesByTitle", title, null, commandType: CommandType.StoredProcedure).AsQueryable();
         return result;
     }
-    internal int InsertEmployee(Employee employee)
+    public int InsertEmployee(Employee employee)
     {
         using IDbConnection connection = new SqlConnection(_config.GetConnectionString("Default"));
         var result = connection.Execute("SpInsertEmployee", employee, null, commandType: CommandType.StoredProcedure);
         return result;
     }
-    internal int UpdateEmployee(Employee employee)
+    public int UpdateEmployee(Employee employee)
     {
         using IDbConnection connection = new SqlConnection(_config.GetConnectionString("Default"));
         var result = connection.Execute("SpUpdateEmployee", employee, null, commandType: CommandType.StoredProcedure);
         return result;
     }
-    internal int DeleteEmployeeRecord(int databaseId)
+    public int DeleteEmployeeRecord(int databaseId)
     {
         using IDbConnection connection = new SqlConnection(_config.GetConnectionString("Default"));
         var result = connection.Execute("SpDeleteEmployeeRecord", databaseId, null, commandType: CommandType.StoredProcedure);
--- EmpDemoApi/Data/IDataAccess.cs
+++ EmpDemoApi/Data/IDataAccess.cs
@@ -1,8 +1,19 @@
 ﻿using Shared.Models;
 
 namespace API.Data;
-
 public interface IDataAccess
 {
-    IQueryable<Employee> GetAllEmployees();
+    int DeleteEmployeeRecord(int databaseId);
+    IQueryable<Employee>? GetAllActiveEmployees();
+    IQueryable<Employee>? GetAllEmployees();
+    IQueryable<Employee>? GetAllInactiveEmployees();
+    IQueryable<Employee>? GetEmployeeByDatabaseId(int databaseId);
+    IQueryable<Employee>? GetEmployeeByEmployeeId(Guid id);
+    IQueryable<Employee>? GetEmployeesByAge(int age, bool greaterThan = false, bool lessThan = false, bool equalTo = true);
+    IQueryable<Employee>? GetEmployeesByHireDate(DateTime hireDate, bool greaterThan = false, bool lessThan = false, bool equalTo = true);
+    IQueryable<Employee>? GetEmployeesByName(string? firstName = null, string? lastName = null);
+    IQueryable<Employee>? GetEmployeesBySalary(decimal salary, bool greaterThan = false, bool lessThan = false, bool equalTo = true);
+    IQueryable<Employee>? GetEmployeesByTitle(string title);
+    int InsertEmployee(Employee employee);
+    int UpdateEmployee(Employee employee);
 }
