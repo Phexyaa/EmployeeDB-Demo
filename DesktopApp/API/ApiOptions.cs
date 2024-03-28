c70eca8adfb55b2147d0ac06a26d0a9c58ccf125
Phexyaa <32746604+Phexyaa@users.noreply.github.com>
2024-03-28 19:38:05 -0400

    Added api options file

---
+++ DesktopApp/API/ApiOptions.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopApp.API;
+internal class ApiOptions : ObservableObject
+{
+    private string? _allowedHost;
+    public string? AllowedHost
+    {
+        get => _allowedHost; set => SetProperty(ref _allowedHost, value);
+    }
+}
