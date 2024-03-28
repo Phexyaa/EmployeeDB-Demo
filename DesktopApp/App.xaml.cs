75b8823105f4eab7948939e4418ea0b21a76fe23
Phexyaa <32746604+Phexyaa@users.noreply.github.com>
2024-03-28 12:05:37 -0400

    Added configuration provider and appsettings.json

--- DesktopApp/App.xaml.cs
+++ DesktopApp/App.xaml.cs
@@ -1,8 +1,13 @@
 ﻿using System.Net.Http;
+using System.Text.Json;
 using System.Windows;
 using DesktopApp.Mock;
+using DesktopApp.Services;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using Shared.Enums;
 using Shared.Global;
 using Shared.Utility;
 
@@ -14,13 +19,15 @@ public partial class App : Application
 {
     public IServiceProvider Services { get; }
     public new static App Current = (App)Application.Current;
+    private static HostApplicationBuilder builder = Host.CreateApplicationBuilder();
+    private static IConfiguration _config => new ConfigurationBuilder()
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .AddEnvironmentVariables()
+        .Build();
 
-    private HostApplicationBuilder builder = Host.CreateApplicationBuilder();
     public App()
     {
         Services = ConfigureServices();
-
-        
     }
 
     private static IServiceProvider ConfigureServices()
@@ -29,6 +36,8 @@ public partial class App : Application
 
         services.AddSingleton<IApiService, MockApi>();
         services.AddSingleton<HttpClient>();
+        services.Configure<Settings>(_config.GetSection("Defaults"));
+        services.AddSingleton(_config);
         services.AddTransient<IEmployeeFactory, MockEmployeeFactory>();
         services.AddTransient<Defaults>();
 
---
+++ DesktopApp/Services/Settings.cs
@@ -0,0 +1,34 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.Extensions.Configuration;
+using Shared.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace DesktopApp.Services;
+public class Settings : ObservableObject
+{
+    private Dictionary<SearchCriteria, string>? _searchCriteriaToString;
+
+    [Required]
+    public Dictionary<SearchCriteria, string>? SearchCriteriaToString
+    {
+        get => _searchCriteriaToString;
+        set => SetProperty(ref _searchCriteriaToString, value);
+    }
+    private List<string>? _employeeTitles;
+
+    [Required]
+    public List<string>? EmployeeTitles
+    {
+        get => _employeeTitles;
+        set => SetProperty(ref _employeeTitles, value);
+    }
+}
