2dfedcd864ef27825dbfa5b092f1894869287f06
Phexyaa <32746604+Phexyaa@users.noreply.github.com>
2024-03-30 14:41:48 -0400

    Added yes/no style confirmation dialog window

---
+++ DesktopApp/Dialogs/IConfirmationDialogViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Input;
+
+namespace DesktopApp.Dialogs;
+internal interface IConfirmationDialogViewModel
+{
+    ICommand AcceptCommand { get; set; }
+    ICommand CancelCommand { get; set; }
+    public EventHandler? UserAccepted { get; set; }
+    public EventHandler? UserRefused { get; set; }
+    public string ConfirmationPrompt { get; set; }
+}
---
+++ DesktopApp/Dialogs/YesNoDialogviewModel.cs
@@ -0,0 +1,44 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DesktopApp.Dialogs
+{
+    class ConfirmationDialogViewModel : ObservableObject, IConfirmationDialogViewModel
+    {
+        public ICommand AcceptCommand { get; set; }
+        public ICommand CancelCommand { get; set; }
+        private string _confirmationPrompt ="";
+        /// <summary>
+        /// The prompt that is displayed to the user.
+        /// </summary>
+        public string ConfirmationPrompt
+        {
+            get => _confirmationPrompt; set => SetProperty(ref _confirmationPrompt, value);
+        }
+
+        /// <summary>
+        /// Event fires whenever a user chooses to accept the prompt.
+        /// </summary>
+        public EventHandler? UserAccepted { get; set; }
+
+        /// <summary>
+        /// Event fires whenever a user refuses the prompt.
+        /// </summary>
+        public EventHandler? UserRefused { get; set; }
+
+        public ConfirmationDialogViewModel()
+        {
+            AcceptCommand = new RelayCommand(HandleUserAccept);
+            CancelCommand = new RelayCommand(HandleUserCancel);
+        }
+
+        private void HandleUserAccept() => UserAccepted?.Invoke(default, new EventArgs());
+        private void HandleUserCancel() => UserRefused?.Invoke(default, new EventArgs());
+    }
+}
